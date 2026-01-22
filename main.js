8dd817295409b103c5fc7210038064443f7a3b9e
MerQuery <ezrapiya.th@gmail.com>
2026-01-22 08:47:34 +0700

    Finishing Temperature Feature

---
+++ main.js
@@ -0,0 +1,11 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/EWX/155,91/forecast");
+  const weatherData = await weatherPromise.json();
+  const ourTemperature = weatherData.properties.periods[0].temperature
+
+  console.log(ourTemperature);
+
+  document.querySelector("#currentTemperature").textContent = ourTemperature;
+}
+
+start();
